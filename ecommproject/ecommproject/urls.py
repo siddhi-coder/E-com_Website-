ed0576a49e2b53d96236b3c4a7cefd48b341d01c
siddhi-coder <gadasiddhi092@gmail.com>
2024-01-12 08:03:47 +0530

    commit

--- ecommproject/ecommproject/urls.py
+++ ecommproject/ecommproject/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from ecommapp import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -26,4 +28,4 @@ urlpatterns = [
     path("userlogout/", views.userlogout, name="userlogout"),
     path("aboutus/", views.aboutus, name="aboutus"),
     path("contactus", views.contactus, name="contactus"),
-]
+]+static(settings.MEDIA_URL , document_root  = settings.MEDIA_ROOT)
