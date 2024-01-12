f3d6252f132350042d77cb1ad66a111da4618aea
siddhi-coder <gadasiddhi092@gmail.com>
2024-01-12 07:58:02 +0530

    commit

--- ecommproject/ecommapp/admin.py
+++ ecommproject/ecommapp/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from models import Cart , Order , Product
 
 # Register your models here.
+class AdminProduct (admin.ModelAdmin):
+    list_display = (
+        "userid" , 
+        "productid",
+        "product_name",
+        "category" , 
+        "description",
+        "price",
+        "image",
+    )
+
+
+class AdminCart (admin.ModelAdmin):
+    list_display = (
+        "userid" , 
+        "productid",
+        "quantity",
+    )
+
+class AdminOrder (admin.ModelAdmin):
+    list_display = (
+        "userid" , 
+        "productid",
+        "orderid",
+        "quantity",
+        "status",
+    )
+
+admin.site.register(Product,AdminProduct)
+admin.site.register(Cart,AdminCart)
+admin.site.register(Order,AdminOrder)
--- ecommproject/ecommapp/models.py
+++ ecommproject/ecommapp/models.py
@@ -1,3 +1,27 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Product(models.Model):
+    userid = models.ForeignKey(User , on_delete = models.CASCADE , null = True , blank = True)
+    productid = models.IntegerField(primary_key = True)
+    product_name = models.CharField(max_length = 55)
+    type = (("Mobile","Mobile"),("Clothes", "Clothes"),("Shoes","Shoes"))
+    category = models.CharField(max_length = 50 ,choices = type , default = "")
+    description = models.TextField(max_length = 100)
+    price = models.FloatField()
+    image=models.ImageField(upload_to='product/')
+
+class Cart(models.Model):
+    userid = models.ForeignKey(User , on_delete = models.CASCADE , null = True , blank = True)
+    productid = models.ManyToManyField(Product,on_delete = models.CASCADE , null = True , blank = True)
+    quantity = models.PositiveIntegerField(default = 0)
+
+
+class Order(models.Model):
+    userid = models.ForeignKey(User , on_delete = models.CASCADE , null = True , blank = True)
+    productid = models.ManyToManyField(Product,on_delete = models.CASCADE , null = True , blank = True)
+    orderid = models.IntegerField(primary_key = True)
+    quantity = models.PositiveIntegerField(default = 0)
+    status = (("Pending" , "Pending") , ("Out for Delivery","Out for Delivery") , ("Deliverd" , "Deliverd"))
+
+
